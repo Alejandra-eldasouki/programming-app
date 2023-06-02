baa612f8768245e89d73c7a0e84c7dc1457711b3
Alejae1998 <alejagr1998@gmail.com>
2023-06-02 13:10:54 -0700

    add questions dynamically

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+const languageSuggestions = [
+  {
+    language: 'Ruby',
+    questions: [
+      'Do you enjoy writing elegant code?',
+      'Do you want to learn about a vibrant open-source community ',
+    ],
+    answers: ['yes', 'yes'],
+  },
+  {
+    language: 'c#',
+    questions: [
+      'Do you want to learn a remarkably scalable and easy to maintain programming language',
+      'Are you interested in learning a language that its syntax resembles human language',
+    ],
+    answers: ['yes', 'yes'],
+  },
+  {
+    language: 'JavaScript',
+    questions: [
+      'Do you want to learn a object-oriented, functional, and imperative styles of programming',
+      'Are you interested in front-end development',
+    ],
+    answers: ['yes', 'yes'],
+  },
+];
